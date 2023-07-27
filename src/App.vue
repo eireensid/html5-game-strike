7bd61b14227039d425c709896e299d8155a0a3a0
Irina <sidoren-irina@yandex.ru>
2023-07-27 11:35:26 +0300

    Add header

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
 <template>
-hello
+  <Header />
+  <div class="wrapper">
+    <canvas width="200" height="200" id="canvas"></canvas>
+  </div>
 </template>
 
 <script setup lang="ts">
+import {onMounted} from "vue";
+import Header from "./components/Header.vue";
+
+onMounted(() => {
+  const canvas = document.querySelector('#canvas')
+  const ctx = canvas.getContext('2d')
+  ctx.fillRect(0, 0, canvas.width, canvas.height)
+})
 </script>
 
 <style scoped>
---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="header">
+    <div class="left"></div>
+    <h1 class="title">Critical Space Strike</h1>
+    <div class="right"></div>
+  </div>
+</template>
+
+<style scoped>
+.header {
+  background: #070F20;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 16px;
+}
+.title {
+  color: #fff;
+  font-weight: 300;
+}
+</style>
