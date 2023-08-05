bb69ef5224ef2fe6f3525b0f4f47794d7436b251
Irina <sidoren-irina@yandex.ru>
2023-08-05 18:01:08 +0300

    Add collision test

--- src/tests/bullet-collision.test.ts
+++ src/tests/bullet-collision.test.ts
@@ -1,16 +1,32 @@
 import GameRules from '@/model/GameRules'
-import { expect, test } from 'vitest'
+import { describe, expect, test } from 'vitest'
 
-test('bullet collision', () => {
-  expect(GameRules.checkCollision({
-    x: 0,
-    y: 0,
-    height: 20,
-    width: 20
-  }, {
-    x: 0,
-    y: 100,
-    height: 50,
-    width: 50
-  })).toBe(false)
+describe("bullet collision", () => {
+  test('no collision', () => {
+    expect(GameRules.checkCollision({
+      x: 0,
+      y: 0,
+      height: 20,
+      width: 20
+    }, {
+      x: 0,
+      y: 100,
+      height: 50,
+      width: 50
+    })).toBe(false)
+  })
+
+  test('collision', () => {
+    expect(GameRules.checkCollision({
+      x: 0,
+      y: 0,
+      height: 20,
+      width: 20
+    }, {
+      x: 0,
+      y: 20,
+      height: 50,
+      width: 50
+    })).toBe(true)
+  })
 })
